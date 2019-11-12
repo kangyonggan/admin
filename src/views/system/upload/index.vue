136af647d71518928a9d08a965312f106860a999
kangyonggan <java@kangyoonggan.com>
2019-11-12 20:02:18 +0800

    upload

---
+++ src/views/system/upload/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-upload
+    :action="axios.defaults.baseURL + 'fileUpload'"
+    :on-success="uploadSuccess"
+  >
+    <i class="el-icon-upload2" />
+  </el-upload>
+</template>
+
+<script>
+    export default {
+        methods: {
+            uploadSuccess() {
+                this.success('上传成功');
+            }
+        }
+    };
+</script>
