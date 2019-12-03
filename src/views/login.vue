c02ce8050a696ce31b2346a54d2c03cb08bacc98
kangyonggan <java@kangyonggan.com>
2019-12-03 10:13:10 +0800

    github login

--- src/views/login.vue
+++ src/views/login.vue
@@ -56,6 +56,17 @@
               src="../assets/images/qq.png"
               @click="qqLogin"
             >
+            <svg
+              height="16"
+              width="16"
+              style="margin-left: 8px;"
+              @click="githubLogin"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+              />
+            </svg>
           </div>
         </el-form>
       </el-card>
@@ -112,9 +123,12 @@
                     redirectURI: 'http://kangyonggan.com:8888/login'
                 });
             },
-            qqCallbackLogin(params) {
+            githubLogin() {
+              window.open('https://github.com/login/oauth/authorize?client_id=0c9e693180e5376a2ae7&redirect_uri=http://kangyonggan.com:8888/login');
+            },
+            callbackLogin(url, params) {
                 this.loading = true;
-                this.axios.post('qqLogin', params).then((data) => {
+                this.axios.post(url, params).then((data) => {
                     this.$store.commit('setUser', data.user);
                     this.$store.commit('setMenus', data.menus);
                     this.$router.push({
@@ -133,7 +147,13 @@
             if (hash) {
                 hash = hash.replace('#', '');
                 let params = qs.parse(hash);
-                this.qqCallbackLogin({'accessToken': params.access_token, 'expiresIn': params.expires_in});
+                if (params.access_token) {
+                    // QQ回调
+                    this.callbackLogin('qqLogin', {'accessToken': params.access_token, 'expiresIn': params.expires_in});
+                } else if (params.code) {
+                    // Github回调
+                    this.callbackLogin('githubLogin', {'code': params.code});
+                }
             }
         }
     };
